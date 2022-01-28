5688e71c623e733f0dcb6e01bc0239c2e67b6c65
Priyanka <priyankabangalorenagaraja@gmail.com>
2022-01-28 16:32:34 +0530

    React Redux Form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
-import {DISHES} from './shared/dishes';
+// import {DISHES} from './shared/dishes';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
-
-
+const store = ConfigureStore();
 
 class App extends Component {
-  constructor(props){
-    super();
-    this.state={
-      dishes : DISHES
-      
-    };
-  }
+  
   render(){
   return (
    
+    <Provider store={store}>
     <BrowserRouter>
       <div className="App">
         <Main />
       </div>
     </BrowserRouter>
+  </Provider>
+
   );
 }
 }
